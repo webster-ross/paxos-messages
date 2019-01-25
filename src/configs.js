946650ab421ca0b057de353b55867337d5e7dfd6
Webster Ross III <webster.ross@me.com>
2019-01-25 09:24:22 -0500

    Add redis caching

--- src/configs.js
+++ src/configs.js
@@ -1,5 +1,6 @@
 export default {
   NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT,
-  DATABASE_URL: process.env.DATABASE_URL
+  DATABASE_URL: process.env.DATABASE_URL,
+  REDIS_URL: process.env.REDIS_URL
 }
--- src/messages.js
+++ src/messages.js
@@ -1,8 +1,11 @@
 import {Router} from 'express'
 import crypto from 'crypto'
+import redis from 'async-redis'
 import pg from './postgres'
+import configs from './configs'
 
 const router = Router()
+const red = redis.createClient(configs.REDIS_URL)
 
 // handle POST /messages
 router.post('/', async (req, res, next) => {
@@ -13,13 +16,21 @@ router.post('/', async (req, res, next) => {
     return res.status(400).send({msg: 'No message specified'})
   }
 
+  // check if hash is in the cache
+  let hash = await red.get(message)
+  if(hash) return res.send({digest: hash})
+
   // create sha256 hash
-  const hash = crypto.createHash('sha256').update(message).digest('hex')
+  hash = crypto.createHash('sha256').update(message).digest('hex')
 
   try {
     // add message & hash to db
     await pg.query(`insert into messages (hash, message) values ($1, $2)
                     on conflict (hash) do nothing`, [hash, message])
+
+    // save message to cache
+    await red.set(hash, message)
+    await red.set(message, hash)
     res.send({digest: hash})
   }
   catch(e) { next(e) }
@@ -30,6 +41,10 @@ router.get('/:hash', async (req, res, next) => {
   const {hash} = req.params
 
   try {
+    // check if message is in the cache
+    const message = await red.get(hash)
+    if(message) return res.send({message: message})
+
     // get message from db by hash
     const {rows: [row]} = await pg.query(`select * from messages where hash = $1`, [hash])
     if(!row) res.status(404).send({msg: 'Message not found'})
